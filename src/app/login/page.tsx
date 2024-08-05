454e48717a5cd1a62009de255b6e126a07a4e772
BeatCo <chrislopes3667@gmail.com>
2024-08-05 16:35:47 +0530

    fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+"use client";
+import { Switch } from "@/components/ui/switch";
+import Link from "next/link";
+import React, { useState } from "react";
 
-const page = () => {
+const Page = () => {
+  const [isSignedin, setSignedin] = useState(true);
+
+  function checkIfSignedin() {
+    setSignedin(false);
+  }
+
+  function handleSignIn(e: any) {
+    e.preventdefault();
+    
+  }
+
   return (
-    <div>reniyas is gay</div>
-  )
-}
+    <>
+      <div className="flex justify-center items-center mx-auto min-w-[100vw] h-screen">
+        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col mt-10 md:mt-0">
+          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+            Sign Up
+          </h2>
+          <form onSubmit={handleSignIn}>
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="password"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <button
+              type="submit"
+              className="text-white w-full mt-3 bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+              Submit
+            </button>
+          </form>
+          <div className="flex flex-row mt-3 ">
+            <Switch onCheckedChange={checkIfSignedin} />
+            <h2 className="pl-2 text-gray-900 text-lg font-medium title-font -translate-y-[3px]">
+              Already signed in
+            </h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default page
+export default Page;
