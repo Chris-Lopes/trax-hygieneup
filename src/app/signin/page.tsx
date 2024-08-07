062901690a040d410b8c6985ef64363eb50510a4
BeatCo <chrislopes3667@gmail.com>
2024-08-07 23:34:34 +0530

    fix

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,18 +1,11 @@
-"use client";
-import { Switch } from "@/components/ui/switch";
+'use client'
 import Link from "next/link";
-import React, { useState } from "react";
 
 const Page = () => {
-  const [isSignedin, setSignedin] = useState(true);
-
-  function checkIfSignedin() {
-    setSignedin(false);
-  }
-
-  async function handleSignIn(e: any) {
+  const handleSignIn = async (e: any) => {
     e.preventDefault();
 
+    const username = e.targer.usename.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
 
@@ -21,12 +14,13 @@ const Page = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ email, password, username }),
     });
 
     if (response.ok) {
       const data = await response.json();
       console.log(data);
+      return data
       // handle successful sign in
     } else {
       console.error("Sign in failed");
@@ -41,7 +35,7 @@ const Page = () => {
           <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
             Sign In
           </h2>
-          <form onSubmit={handleSignIn}>
+          <form onSubmit={handleSignIn} method="POST">
             <div className="relative mb-4">
               <label
                 htmlFor="username"
@@ -84,7 +78,7 @@ const Page = () => {
                 className="w-full bg-white rounded border border-gray-300 focus:border-black focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <Link href={""}>
+            <Link href={"/"}>
               <button
                 type="submit"
                 className="text-white w-full mt-3 bg-black border-0 py-2 px-8 focus:outline-none hover:bg-slate-600 rounded text-lg"
